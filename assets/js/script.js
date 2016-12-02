38caa71f1dd8a0de6b7c863c6db65d6528833970
Flavio Sena <sena@iocode.com.br>
2016-12-02 11:02:02 -0200

    db list test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,27 @@
-console.log(ipsum_list);
+window.onload = function() {
+    load_ipsum_list();
+};
+
+function load_ipsum_list() {
+    for(i in ipsum_list) {
+        ipsum_package(ipsum_list[i]);
+    }
+}
+
+function ipsum_package(ipsum) {
+    var data = null;
+
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener('readystatechange', function () {
+        if (this.readyState === 4) {''
+            console.log(this.responseText);
+        }
+    });
+
+    xhr.open('GET', 'db/' + ipsum + '/package.json');
+    xhr.setRequestHeader('cache-control', 'no-cache');
+
+    xhr.send(data);
+}
