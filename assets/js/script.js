4642de03043a6925328eaf096c0a66a665c597bc
Flavio Sena <sena@iocode.com.br>
2016-12-02 23:47:11 -0200

    retrieve ipsum

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+var ipsum = [];
 window.onload = function() {
     load_ipsum_list();
 };
@@ -55,4 +56,48 @@ function get_language(package)
         }
     }
     return package;
+}
+function retrieve_ipsum(ipsum_name, lang, limit) {
+    lang = lang ? lang : 'en';
+    limit = limit ? limit : 1;
+
+    var data_list = ipsum_list[ipsum_name].lang[lang];
+
+    if(!data_list) {
+        throw "package error";
+    }
+
+    for (i = 0; i < limit; i++) {
+        var json = fetch_random(data_list);
+        fetch_json(ipsum_name, lang, json);
+    }
+
+}
+function fetch_random(obj) {
+    var temp_key, keys = [];
+    for(temp_key in obj) {
+        if(obj.hasOwnProperty(temp_key)) {
+            keys.push(temp_key);
+        }
+    }
+    return obj[keys[Math.floor(Math.random() * keys.length)]];
+}
+function fetch_json(ipsum_name, lang, json) {
+    var data = null;
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.addEventListener('readystatechange', function () {
+        if (this.readyState === 4) {
+            var json = this.responseText;
+            json = JSON.parse(json);
+            ipsum.push(json.description)
+
+            console.log(ipsum);
+        }
+    });
+
+    xhr.open('GET', 'db/' + ipsum_name + '/' + lang + '/' + json + '.json');
+    xhr.setRequestHeader('cache-control', 'no-cache');
+    xhr.send(data);
 }
