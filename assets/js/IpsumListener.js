7dc7be3feb90824f4b70e28966366c7669f91ee1
Flavio Sena <sena@iocode.com.br>
2017-01-28 21:53:14 -0200

    Redudant code

--- assets/js/IpsumListener.js
+++ assets/js/IpsumListener.js
@@ -1,23 +1,17 @@
 "use strict"
 var IpsumListener = (function (_doc, _window, _ipsumGen) {
 
-    var _btnGenerator = _doc.getElementById('btn-generate'),
-        _formGenerator = _doc.getElementById('form-generate'),
+    var _formGenerator = _doc.getElementById('form-generate'),
         _paragNumInput = _doc.getElementById('paragraph-number-input');
 
     /**
      * Add all the listeners here
      */
-    _btnGenerator.addEventListener("click", generateIpsum, false);
     _formGenerator.addEventListener("submit", onSubmit, false);
 
 
     function onSubmit(e) {
         e.preventDefault();
-        generateIpsum();
-    }
-
-    function generateIpsum() {
         _ipsumGen.genIpsum(_paragNumInput.value);
     }
 
