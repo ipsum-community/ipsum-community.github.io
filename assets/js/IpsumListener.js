d429c71e6904171fae0199bf615c83dcd6508470
Flavio Sena <sena@iocode.com.br>
2017-01-28 01:38:28 -0200

    close #17

--- assets/js/IpsumListener.js
+++ assets/js/IpsumListener.js
@@ -3,14 +3,20 @@ var IpsumListener = (function (_doc, _window, IpsumGenerator) {
 
     var _ipsumGen = IpsumGenerator,
         _btnGenerator = _doc.getElementById('btn-generate'),
+        _frmGenerator = _doc.getElementById('form-generate'),
         _paragNumInput = _doc.getElementById('paragraph-number-input');
 
     /**
      * Add all the listeners here
      */
     _btnGenerator.addEventListener("click", generateIpsum, false);
+    _frmGenerator.addEventListener("submit", onSubmit, false);
 
 
+    function onSubmit(e) {
+        e.preventDefault();
+        generateIpsum();
+    }
     function generateIpsum() {
         _ipsumGen.genIpsum(_paragNumInput.value);
     }
