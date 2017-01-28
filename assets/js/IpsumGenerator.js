e254de9714dbf9f5001aeda278cfe1410579e0df
ramon183 <ramonroc@gmail.com>
2017-01-28 20:25:12 -0200

    IpsumGenerator refactored

--- assets/js/IpsumGenerator.js
+++ assets/js/IpsumGenerator.js
@@ -1,5 +1,5 @@
 "use strict"
-var IpsumGenerator = (function (_doc, _window) {
+var IpsumGenerator = (function (_doc, _window, _util) {
 
     var _container = _doc.getElementById('phrase-container');
 
@@ -11,35 +11,19 @@ var IpsumGenerator = (function (_doc, _window) {
         _container.innerHTML += paragraph;
     }
 
-    function capitalizeFirstLetter(string) {
-        return string.charAt(0).toUpperCase() + string.slice(1);
-    }
-
-    function fetchRandom(obj) {
-        var temp_key, keys = [];
-        for (temp_key in obj) {
-            keys.push(temp_key);
-        }
-        return obj[keys[Math.floor(Math.random() * keys.length)]];
-    }
-
-    function getRandomInt(min, max) {
-        return Math.floor(Math.random() * (max - min)) + min;
-    }
-
     function generateIpsum(numberOfParagraphs) {
         var i, j, numberOfParagToMakeOne, paragraph;
 
         clearContainer();
         for (i = 0; i < numberOfParagraphs; i++) {
             paragraph = '';
-            numberOfParagToMakeOne = getRandomInt(2, 10);
+            numberOfParagToMakeOne = _util.randomNumberBetween(2, 10);
 
             for (j = 0; j < numberOfParagToMakeOne; j++) {
-                paragraph += ' ' + fetchRandom(_window.store[0].text);
+                paragraph += ' ' + _util.fetchRandom(_window.store[0].text);
             }
 
-            addToContainer('<p>' + capitalizeFirstLetter(paragraph) + '</p>');
+            addToContainer('<p>' + _util.capitalizeFirstLetter(paragraph) + '</p>');
         }
     }
 
@@ -49,4 +33,4 @@ var IpsumGenerator = (function (_doc, _window) {
             generateIpsum(number);
         }
     }
-})(document, window);
+})(document, window, Util);
---
+++ assets/js/Util.js
@@ -0,0 +1,19 @@
+"use strict"
+var Util = (function (_math) {
+
+    return {
+        capitalizeFirstLetter: function (string) {
+            return string.charAt(0).toUpperCase() + string.slice(1);
+        },
+        randomNumberBetween: function (min, max) {
+            return _math.floor(_math.random() * (max - min)) + min;
+        },
+        fetchRandom: function (obj) {
+            var temp_key, keys = [];
+            for (temp_key in obj) {
+                keys.push(temp_key);
+            }
+            return obj[keys[_math.floor(_math.random() * keys.length)]];
+        }
+    }
+})(Math);
