4f4cc9cbbbe59ff4187d3fd2cd5289a27d506030
Flavio Sena <sena@iocode.com.br>
2017-01-27 22:48:04 -0200

    #2 load paragraphs.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,33 @@
-window.onload = function() {
+window.onload = function () {
 };
+
+function loadParagraphs(limit) {
+    limit = limit ? limit : 1;
+
+
+    var container = document.getElementById('phrase-container');
+    container.innerHTML = '';
+
+    for (i = 0; i < limit; i++) {
+        var pLimit = getRandomInt(2, 20);
+        var p = ''
+
+        for (j = 0; j < pLimit; j++) {
+            p = p + fetchRandom(window.store[0].text);
+        }
+        container.innerHTML = container.innerHTML + '<p>' + p + '</p>';
+    }
+}
+function fetchRandom(obj) {
+    var temp_key, keys = [];
+    for (temp_key in obj) {
+        if (obj.hasOwnProperty(temp_key)) {
+            keys.push(temp_key);
+        }
+    }
+    return obj[keys[Math.floor(Math.random() * keys.length)]];
+}
+//https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
